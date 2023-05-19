a190b41793673d518e29b970420ee9226ae53584
Clarc Infante <clarence.infante@quincus.com>
2023-05-19 14:40:34 +0800

    Update LegalSightApplication.java

--- src/main/java/com/legalsight/LegalSightApplication.java
+++ src/main/java/com/legalsight/LegalSightApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LegalSightApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LegalSightApplication.class, args);
     }
-
 }
